c95d8fa56a1db92b59d2a59c19ecc34675ded0e1
Paul Thompson <pmt10@case.edu>
2024-04-09 13:19:53 -0400

    find low and high ttls

--- data_wrangling/ttls.py
+++ data_wrangling/ttls.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def find_high_ttls_at_single_channel(digital_inputs, ttl_index):
     """
 
@@ -17,9 +20,56 @@ def find_high_ttls_at_single_channel(digital_inputs, ttl_index):
 
     # ERROR CHECKING
     # Make sure ttl_index is not larger than number of bits in digital input
-    # type (does this matter if python doens't care so much about types?)
+    # type (does this matter if python doesn't care so much about types?)
 
     binary_ttl_mask = 2**ttl_index
     ttl_boolean = ((digital_inputs & binary_ttl_mask) > 0).astype(int)
 
     return ttl_boolean
+
+
+def is_ttl_transition_low_to_high(ttl_boolean):
+    """
+
+    Parameters
+    ----------
+    ttl_boolean
+
+    Returns
+    -------
+
+    """
+
+    total_high_ttl = np.count_nonzero(ttl_boolean)
+    total_low_ttl = len(ttl_boolean) - total_high_ttl
+
+    print("Total High TTL Values: ", total_high_ttl)
+    print("Total Low TTL Values: ", total_low_ttl)
+    if total_high_ttl > total_low_ttl:
+        print("Events are most likely to be on high to low transition")
+        return False
+    else:
+        print("Events are likely to be on low to high transition")
+        return True
+
+
+def get_low_to_high_transitions(ttl_boolean):
+
+    # ERROR CHECKING
+    # Values not equal to 0 and 1
+
+    ttl_boolean_diff = np.ediff1d(ttl_boolean)
+    ttl_ticks = np.where(ttl_boolean_diff > 0)[0] - 1
+
+    return ttl_ticks
+
+
+def get_high_to_low_transitions(ttl_boolean):
+
+    # ERROR CHECKING
+    # Values not equal to 0 and 1
+
+    ttl_boolean_diff = np.ediff1d(ttl_boolean)
+    ttl_ticks = np.where(ttl_boolean_diff < 0)[0] - 1
+
+    return ttl_ticks
