436696527ceb653b71d59c5e339ff44a0fb659f3
Paul Thompson <pmt10@case.edu>
2024-07-29 13:28:30 -0400

    intan output will return off and on signals

--- data_wrangling/intan.py
+++ data_wrangling/intan.py
@@ -1,10 +1,15 @@
-import numpy as np
 
 from .binary_data import get_digital
 from .ttls import get_ttl_timestamps_16bit
 
+import numpy as np
 
-def get_camera_ttl_array(intan_digital_filepath, ttl_index=1):
+
+def get_camera_ttl_array(
+    intan_digital_filepath,
+    ttl_index=1,
+    isTransitionLowToHigh=None,
+):
     """
     Intan stores 16 digital inputs in one 16-bit number per timestamp sampled at
     the system sampling rate (usually 30,000 Hz). Here we first find the binary
@@ -15,11 +20,17 @@ def get_camera_ttl_array(intan_digital_filepath, ttl_index=1):
     ----------
     intan_digital_filepath: string
     ttl_index: int
-
+        zero based index (0-15). This is the index of the TTL
+        channel that we are interested in
+    isTransitionLowToHigh: bool or None
+        If None, the function will attempt to determine
+        the transition type from the data
     Returns
     -------
     np.ndarray[int]:
-        Time (in ticks) where ttl at index transitions from low to high
+        Time (in ticks) where ttl at index transitions ON
+    np.ndarray[int]:
+        Time (in ticks) where ttl at index transitions OFF
     """
     digital_inputs = get_digital(
         intan_digital_filepath,
@@ -27,9 +38,12 @@ def get_camera_ttl_array(intan_digital_filepath, ttl_index=1):
         2,
     )
 
-    ttl_onsets, ttl_offsets = get_ttl_timestamps_16bit(digital_inputs, ttl_index)
+    ttl_onsets, ttl_offsets = get_ttl_timestamps_16bit(
+        digital_inputs,
+        ttl_index,
+        isTransitionLowToHigh,)
 
-    return ttl_onsets
+    return ttl_onsets, ttl_offsets
 
 
 def load_voltage(voltage_filepath, channel_count):
---
+++ tests/data_wrangling/test_intan.py
@@ -0,0 +1,22 @@
+import pytest
+
+from data_wrangling.intan import get_camera_ttl_array
+
+
+"""
+def test_get_camera_ttl_array():
+
+    intan_digital_filepath = "tests/data_wrangling/data/digitalin1.dat"
+    ttl_index = 0
+
+    ttl_onsets, ttl_offsets = get_camera_ttl_array(
+        intan_digital_filepath,
+        ttl_index,
+    )
+
+    # Expected results
+    num_expected_onsets = 1000
+
+    assert ttl_onsets.shape[0] == num_expected_onsets
+
+"""
