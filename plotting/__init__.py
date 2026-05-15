6368e69d7dadb46c01f9dfef436b7c7ed3b8d84a
Paul Thompson <pmt10@case.edu>
2026-05-15 16:39:41 -0400

    add raster layout methods that are used when there are more rows than pixels

--- plotting/__init__.py
+++ plotting/__init__.py
@@ -3,10 +3,22 @@
 from .broken_axes import add_vertical_broken_axis_style
 from .grid_axes import style_x_axis_with_dedicated_label_row
 from .psth_raster import plot_psth, plot_raster
+from .raster_layout import (
+    bin_spike_lists,
+    effective_raster_linelength,
+    lineoffsets_equal_height_band,
+    merge_spikes_chunk,
+    sorted_subset_trials,
+)
 
 __all__ = [
     "add_vertical_broken_axis_style",
+    "bin_spike_lists",
+    "effective_raster_linelength",
+    "lineoffsets_equal_height_band",
+    "merge_spikes_chunk",
     "plot_psth",
     "plot_raster",
+    "sorted_subset_trials",
     "style_x_axis_with_dedicated_label_row",
 ]
